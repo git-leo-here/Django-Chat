36898dee3965311fe28318a545104a957fc8c159
git-leo-here <ankit2003.ar15@gmail.com>
2024-02-09 20:12:04 +0530

    Completed v1.0.0

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,6 +1,6 @@
 import json
 from channels.db import database_sync_to_async
-from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from .models import Message
  
 # class ChatConsumer(AsyncWebsocketConsumer):
@@ -21,41 +21,66 @@ from .models import Message
 #                 "message" : message , 
 #                 "username" : username ,
 #             })
-#     async def sendMessage(self , event) : 
-#         message = event["message"]
-#         username = event["username"]
-#         await self.send(text_data = json.dumps({"message":message ,"username":username}))
+    # async def sendMessage(self , event) : 
+    #     message = event["message"]
+    #     username = event["username"]
+    #     await self.send(text_data = json.dumps({"message":message ,"username":username}))
 
 
-class ChatConsumer(AsyncJsonWebsocketConsumer):
+class ChatConsumer(AsyncWebsocketConsumer):
     
 
     async def connect(self):
+        self.roomGroupName = "chat_room"
+        await self.channel_layer.group_add(
+            self.roomGroupName ,
+            self.channel_name
+        )
         print("Connected")
         await self.accept()
 
     async def disconnect(self, close_code):
         print("Disconnected")
+        await self.channel_layer.group_discard(
+            self.roomGroupName ,
+            self.channel_layer
+        )
 
-    async def receive_json(self, content):
-        message = content['message']
-        username = content['username']
+    async def receive(self, text_data):
+        print("Received")
+        text_data = json.loads(text_data)
+        message = text_data['message']
+        username = text_data['username']
 
         # Save the message to the database
         await self.save_message(message, username)
 
-        # Get all the messages in the table
-        messages = await self.get_messages()
+        await self.channel_layer.group_send(
+            self.roomGroupName,{
+                "type" : "sendMessage" ,
+                "message" : message , 
+                "username" : username ,
+            })
 
-        # Send the messages back to the WebSocket
-        await self.send_json(list(messages))
+        # # Get all the messages in the table
+        # messages = await self.get_messages()
+
+        # # Send the messages back to the WebSocket
+        # await self.send_json(list(messages))
+    
+    async def sendMessage(self , event) : 
+        message = event["message"]
+        username = event["username"]
+        await self.send(text_data = json.dumps({"message":message ,"username":username}))
 
     @database_sync_to_async
     def save_message(self, message, username):
         # This is a synchronous function
-        Message.objects.create(message=message, username=username)
+        # Message.objects.create(message=message, username=username)
+        msg = Message(username=username , message = message)
+        msg.save()
 
-    @database_sync_to_async
-    def get_messages(self):
-        # This is a synchronous function
-        return list(Message.objects.values('message', 'username'))
+    # @database_sync_to_async
+    # def get_messages(self):
+    #     # This is a synchronous function
+    #     return list(Message.objects.values('message', 'username'))
--- django_chat/settings.py
+++ django_chat/settings.py
@@ -82,10 +82,6 @@ WSGI_APPLICATION = 'django_chat.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_chat',
